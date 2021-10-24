c82ffd97ec8f96857682bc9f7a3cb32c66ff2e7b
Han <han@jvdiamondtech.com>
2021-10-24 11:52:16 +0800

    feat: 模組化講解

--- use-js/main.js
+++ use-js/main.js
@@ -1,6 +1,9 @@
 // import _ from "lodash"
 const _ = require("lodash");
 
+const { text } = require("./a");
+
 const r = _.sortedIndex([30, 50], 40);
 
 console.log("What is the result:", r);
+console.log("What is a:", text);
